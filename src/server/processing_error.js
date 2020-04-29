297bd665c7a443fbdcb5e0db0a22df8a606e826e
Sceat <sceat@hydre.io>
2020-04-29 04:27:20 +0200

    WIP: heavily moving stuff arround

--- src/server/processing_error.js
+++ src/server/processing_error.js
@@ -1,10 +1,19 @@
 import graphql_error from 'graphql/error'
 
-const { GraphQLError } = graphql_error
+const {
+  GraphQLError,
+} = graphql_error
 
 export default class Processing_error extends GraphQLError {
-  constructor({ id, operation_name, operation_type }, raw_graphql_error) {
+  constructor({
+    id, operation_name, operation_type,
+  }, raw_graphql_error) {
     super('processing error')
-    Object.assign(this, { ...raw_graphql_error, id, operation_name, operation_type })
+    Object.assign(this, {
+      ...raw_graphql_error,
+      id,
+      operation_name,
+      operation_type,
+    })
   }
 }
